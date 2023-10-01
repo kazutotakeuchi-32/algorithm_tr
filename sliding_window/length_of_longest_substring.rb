c49a50bcfca6126c056a60a37d3aae4c3ba6893d
竹内和人 <takeuchikazuto@takeuchikazutonoMacBook-Pro.local>
2023-10-01 14:16:00 +0900

    sliding_window length_of_longest_substring clear

--- sliding_window/length_of_longest_substring.rb
+++ sliding_window/length_of_longest_substring.rb
@@ -1,2 +1,24 @@
+# @see https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions
+# @param {String} s
+# @return {Integer}
 def length_of_longest_substring(s)
-end
+    charset = {}
+    l = 0
+    res = 0
+    r = 0
+
+    while r < s.length 
+        while charset.include?(s[r]) 
+            charset.delete(s[l])
+            l+=1
+        end
+
+        charset[s[r]] = true
+
+        res = [res, r - l + 1].max
+        r += 1
+    end
+
+    return res
+
+end 
