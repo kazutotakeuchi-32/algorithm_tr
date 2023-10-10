d82e973206ea918fab18503bdbb4454e77e792f0
竹内和人 <takeuchikazuto@takeuchikazutonoMacBook-Pro.local>
2023-10-10 22:56:26 +0900

    array_hasing#two_sum update

--- array_hasing/two_sum.rb
+++ array_hasing/two_sum.rb
@@ -3,6 +3,8 @@
 # @return {Integer[]}
 # @see https://leetcode.com/problems/two-sum/description/
 
+# 計算量: n**2
+# target == nums[i] + nums[j]
 def two_sum(nums, target)
     i = 0
     while i < nums.length - 1
@@ -15,5 +17,22 @@ def two_sum(nums, target)
         end
         i+=1
     end
+    []
+end
+
+
+#計算量： n
+# (target == nums[l] + nums[r]) == (target - nums[l] == nums[r])
+
+def two_sum(nums, target)
+    i = 0
+    hash = {}
+    while i < nums.length
+       return [hash[nums[i]], i] if hash[nums[i]]
+
+        hash[target - nums[i]] = i
+        i += 1
+    end
+
     []
 end
